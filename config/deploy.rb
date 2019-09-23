8334f11a70d2b6b373c79054ec7694c5bf8431e6
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-23 12:26:33 +0900

    uploadNameEditCommit

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ lock "~> 3.11.1"
 set :application, 'freemarket_sample_59c'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, 'git@github.com:kurochaaaaan/freemarket_sample_59c.git'
+set :repo_url, 'git@github.com:ramuneru/freemarket_sample_59c.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
