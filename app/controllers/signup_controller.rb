907a8855062058c0a1c0a26f21e95877bca70f4b
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-26 18:52:53 +0900

    sigUpCommit

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,4 @@
 class SignupController < ApplicationController
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  get 'users/index'
+  resources :users , only: [:index]
+  resources :signup, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
