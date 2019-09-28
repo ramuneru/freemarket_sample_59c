5c06db6a5c6ba46533537210cc7f7248522d4073
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-28 17:12:48 +0900

    SigUpCommit

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,11 @@
 class SignupController < ApplicationController
-  def new
+  layout 'application.user', except: [:profile]
+
+  def index
+  end
+
+  def profile
+    render layout: 'application.user'
+    @user = User.new
   end
 end
