7938fdfc362856bd6e38fb7bc770253cdc06363c
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-09-28 21:47:41 +0900

    retryCommit

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -5,7 +5,26 @@ class SignupController < ApplicationController
   end
 
   def profile
-    render layout: 'application.user'
     @user = User.new
+    render layout: 'application.user'
   end
+
+  def phone
+  end
+
+  def phone_authen
+  end
+
+  def address
+  end
+
+  def credit
+  end
+
+  def complete
+  end
+
+  private
+    def user_params
+    end
 end
