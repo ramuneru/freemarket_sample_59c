08afa72e4f9f9745d73edbd872e3fba225717011
ramuneru <accuracy.precision.continuum@gmail.com>
2019-09-28 14:32:59 +0900

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2019_09_25_051020) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -23,4 +24,5 @@ ActiveRecord::Schema.define(version: 2019_09_25_051020) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
