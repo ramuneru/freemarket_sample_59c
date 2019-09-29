ae48d6f27783db5be24468526cf134fb64d62d8e
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-09-29 15:30:28 +0900

    jquery tab

--- app/assets/javascripts/channels/users.js
+++ app/assets/javascripts/channels/users.js
@@ -1,39 +1,21 @@
-$(function() {
 
-  // ①タブをクリックしたら発動
-  $('.main-contents__p__list li').click(function() {
 
-    // ②クリックされたタブの順番を変数に格納
-    var index = $('.main-contents__p__list li').index(this);
-
-    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
-    $('.main-contents__p__list li').removeClass('active');
-// --------------------------------------------------------------------÷
-    // ④クリックされたタブにクリック済みデザインを適用する
-    $(this).addClass('active');
-
-    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
-    $('.lists ul').removeClass('show').eq(index).addClass('show');
-
-  });
-});
 
 $(function() {
 
-  // ①タブをクリックしたら発動
-  $('.main-contents__p__list li').click(function() {
+	$('.main-contents__p__list li:nth-child(1)').addClass('active');
 
-    // ②クリックされたタブの順番を変数に格納
-    var index = $('.main-contents__p__list li').index(this);
+	$('.main-contents__p__list li').click(function() {
 
-    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
-    $('.main-contents__p__list li').removeClass('active');
-// --------------------------------------------------------------------÷
-    // ④クリックされたタブにクリック済みデザインを適用する
-    $(this).addClass('active');
+		var index = $(this).parent().children('li').index(this);
 
-    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
-    $('.lists ul').removeClass('show').eq(index).addClass('show');
+		$(this).parent('.main-contents__p__list').each(function(){
 
-  });
+			$('>li',this).removeClass('active').eq(index).addClass('active');
+
+		});
+
+		$(this).parent().next().children('.lists ul').hide().eq(index).show();
+
+	}).first().click();
 });
