91f026eef9286bf646f0c54a523f34e4a205ba5d
ramuneru <accuracy.precision.continuum@gmail.com>
2019-10-01 20:28:57 +0900

    Initial-commit
    商品出品ページ作成開始

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,27 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(params_new)
+  end
+
+  def edit
+    render layout: 'application.user'
+
+  end
+
 
   def show
   end
+
+
+
+  private
+  def params_new
+    params.require(:item).permit(:title, :description, :category_id, :brand_id, :status, :size_id, :region, :shipping_fee_burden, :shipping_method, :region, :shipping_duration, :price, images_attributes: [:image])
+  end
+
 end
