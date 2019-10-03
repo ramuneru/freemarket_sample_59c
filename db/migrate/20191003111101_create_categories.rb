63e2c4da3421506abfc04fb4b4c23edd6b2a5aec
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-10-03 21:49:45 +0900

    AddLoginlinkcommit

--- db/migrate/20191003111101_create_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategories < ActiveRecord::Migration[5.2]
-  def change
-    create_table :categories do |t|
-      t.string :name, null: false
-      t.references :size, foreign_key: true
-      t.timestamps
-    end
-  end
-end
