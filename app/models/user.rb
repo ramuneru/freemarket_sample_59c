1be69e3f1c084527259a1fb23b09447c2f92325a
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-10-08 20:16:54 +0900

    user.rbEditCommit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable  
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable,
-          :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
-          
+  :recoverable, :rememberable, :validatable,
+  :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
   has_one :user_profile, dependent: :destroy
   accepts_nested_attributes_for :user_profile
 
