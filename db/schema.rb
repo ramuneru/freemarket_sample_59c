9dff66ddad86f39f4364dad0a0193bee48abbd43
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-10-08 19:51:19 +0900

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 2019_10_06_080920) do
-=======
 
 ActiveRecord::Schema.define(version: 2019_10_07_035951) do
->>>>>>> Stashed changes
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
