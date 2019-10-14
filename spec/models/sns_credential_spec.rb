8b8b51cda219488555fa3534fb5d3cde14d23fd8
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-10-14 12:23:50 +0900

    sns_credential_test

--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -6,5 +6,17 @@ RSpec.describe SnsCredential, type: :model do
       sns_credential = build(:sns_credential)
       expect(sns_credential).to be_valid
     end
+
+    it "uidがないと作成できない" do
+      sns_credential = build(:sns_credential, uid: "")
+      sns_credential.valid?
+      expect(.errors[:uid]).to include("can't be blank")
+    end
+
+    it "providerがないと作成できない" do
+      sns_credential = build(:sns_credential, provider: "")
+      sns_credential.valid?
+      expect(.errors[:uid]).to include("can't be blank")
+    end
   end
 end
