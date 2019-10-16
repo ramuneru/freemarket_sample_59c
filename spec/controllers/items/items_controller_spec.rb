aec5298424ce1acf126be2bba40d8616f1f4139a
akitaka718 <rsp81183@gmail.com>
2019-10-16 20:55:36 +0900

    controller_test

---
+++ spec/controllers/items/items_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+describe ItemsController do
+  describe 'Delete #destory' do
+    it 'redirect_to_302' do
+      item = create(:item)
+      delete :destroy, params: {id: item}
+      expect(response.status).to eq 302
+    end
+    it 'delete_the_item' do
+      item = create(:item)
+      binding.pry
+      expect{delete :destroy, params: {id: item}}.to change(Item, :count).by(-1)
+    end
+  end
+end
