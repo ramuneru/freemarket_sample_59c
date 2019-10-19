d619884a60929f8e134f9e06529902b7c32829e5
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-10-19 16:37:35 +0900

    CurrentSetUp

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_ancestry
-  has_many :items
+  # has_ancestry
+  # has_many :items
   # アソシエーションはhas_manyではなくbelongs_toだった
-  belongs_to :size
+  # belongs_to :size
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :shipping_fee_burden
-  belongs_to_active_hash :shipping_method
-  belongs_to_active_hash :shipping_duration
-  belongs_to     :user, optional: true
-  belongs_to     :category, dependent: :destroy
-  belongs_to     :size, optional: true
-  has_many       :images, dependent: :destroy
-  validates      :images, presence: true
+  # belongs_to_active_hash :prefecture
+  # belongs_to_active_hash :condition
+  # belongs_to_active_hash :shipping_fee_burden
+  # belongs_to_active_hash :shipping_method
+  # belongs_to_active_hash :shipping_duration
+  # belongs_to     :user, optional: true
+  # belongs_to     :category, dependent: :destroy
+  # belongs_to     :size, optional: true
+  # has_many       :images, dependent: :destroy
+  # validates      :images, presence: true
 
-  accepts_nested_attributes_for :images
-  enum status: [:listing, :progress, :complete]
+  # accepts_nested_attributes_for :images
+  # enum status: [:listing, :progress, :complete]
 end
