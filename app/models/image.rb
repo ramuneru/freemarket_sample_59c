b65fbdc6ca12dbb646b1c05f95ef56c1db2adb2a
KuroChaaaaan <kisaking.onasu@gmail.com>
2019-10-19 16:50:49 +0900

    CurrentSetUpCommit

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  # mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
   # validates      :item_id, presence: true
-  # belongs_to     :item, optional: true
+  belongs_to     :item, optional: true
 end
