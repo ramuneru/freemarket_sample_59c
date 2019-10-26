4ae6b5868012ef0d8ef3c19072c38c5d656c9b68
ramuneru <accuracy.precision.continuum@gmail.com>
2019-10-26 17:10:37 +0900

    edit-page-validation&render-page-modified
    編集ページのバリデーション付加
    renderでの遷移後、各値を出力
    編集における商品のサイズ有→無、無→有のupdateを可能にする

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,19 +14,26 @@ class Item < ApplicationRecord
   validates_presence_of  :images, message: "を登録してください"
   
   validates_presence_of  :title, :description
-  validates_presence_of  :category, :condition, :prefecture
+  validates_presence_of  :condition, :prefecture
+  validates_presence_of  :category
+
+
+
   validates_presence_of  :shipping_fee_burden, :shipping_method, :shipping_duration
   
   validates      :title, length:{ maximum: 40, message: 'を40文字以内で入力してください'} 
   validates      :price, presence: true, numericality: { only_integer: true, greater_than: 300, less_than: 10000000, message: "を300円以上、9,999,999円以下で半角入力してください"}
 
- 
-
   accepts_nested_attributes_for :images
   enum status: [:listing, :progress, :complete]
 
-  # validates      :size, presence: true, if: :category_with_size?
-  # def category_with_size?
-  #   return true if  category_id.include?(:size_id)
-  # end
+
+  validate :category_with_size, if: :category_id
+
+  def category_with_size
+    if category.size_id?
+      errors.add(:size, "を選択してください") unless size
+    end
+  end
+
 end
