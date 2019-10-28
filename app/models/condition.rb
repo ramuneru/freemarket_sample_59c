91746c0347658268f1975187a644db7b3ca2700b
ramuneru <accuracy.precision.continuum@gmail.com>
2019-10-28 13:51:49 +0900

    Fixed-validations
    seedファイル実行にともない、コメントアウトしたvalidationsを元に戻しました

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -4,9 +4,3 @@ class Condition < ActiveHash::Base
     {id: 4, condition: 'やや傷や汚れあり'},{id: 5, condition: '傷や汚れあり'},{id: 6, condition: '全体的に状態が悪い'}
   ]
 end
-
-# @regions = Prefecture.all
-# @conditions = Condition.all
-# @shipping_fee_burdens = Shipping_fee_burden.all
-# @shipping_methods = Shipping_method.all
-# @shipping_durations = Shipping_duration.all
